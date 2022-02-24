ac735be723a83cd5b0e0b4347ed537f418cfa2b0
stephanus yogi <stephanusyogi12@gmail.com>
2022-02-24 22:06:52 +0700

    First Commit

--- application/config/database.php
+++ application/config/database.php
@@ -75,8 +75,8 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
+	'hostname' => 'u7164238_superadmin',
+	'username' => 'superadmin1234',
 	'password' => '',
 	'database' => '',
 	'dbdriver' => 'mysqli',
